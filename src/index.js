dc81a754f5a0105aea0a5363f654497996c7cdc0
Cody-Price <ctprice11@gmail.com>
2019-04-18 17:06:53 -0600

    Test Article

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ErrorPage from './components/ErrorPage/ErrorPage'
+import './containers/Article/Article'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
@@ -20,7 +20,7 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 // 	</Provider>
 // )
 
-ReactDOM.render(ErrorPage, document.getElementById('root'));
+ReactDOM.render(Article, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
